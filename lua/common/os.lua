402801c57807457e3f50e5bafb90259517d36591
Yutong Huang <yutong.huang@outlook.com>
2024-11-27 09:52:36 +0800

    fix windows project root matching bug

--- lua/common/os.lua
+++ lua/common/os.lua
@@ -16,7 +16,14 @@ local detect_os_type = function()
   end
 end
 
+local is_windows = vim.fn.has 'win32' or vim.fn.has 'win64'
+local is_mac = vim.fn.has 'mac'
+local is_linux = vim.fn.has 'linux'
+
 return {
   OS = os_types,
   os_type = detect_os_type,
+  is_linux = is_linux,
+  is_windows = is_windows,
+  is_mac = is_mac,
 }
--- lua/custom/plugins/project.lua
+++ lua/custom/plugins/project.lua
@@ -1,3 +1,32 @@
+local os = require 'common.os'
+
+local config = {}
+
+if os.is_windows then -- pattern matching on windows is broken
+  config = {
+    require('telescope').load_extension 'projects',
+    vim.keymap.set('n', '<leader>ps', function()
+      require('telescope').extensions.projects.projects {}
+    end, { desc = '[P]roject [S]earch' }),
+  }
+else
+  config = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    detection_methods = { 'pattern' },
+
+    -- All the patterns used to detect root dir, when **"pattern"** is in
+    -- detection_methods
+    patterns = { '!../git', 'compile_commands.json', 'build/compile_commands.json', '^.config/nvim' },
+
+    require('telescope').load_extension 'projects',
+    vim.keymap.set('n', '<leader>ps', function()
+      require('telescope').extensions.projects.projects {}
+    end, { desc = '[P]roject [S]earch' }),
+  }
+end
+
 return {
   {
     'ahmedkhalf/project.nvim',
@@ -5,21 +34,7 @@ return {
       'nvim-telescope/telescope.nvim',
     },
     config = function()
-      require('project_nvim').setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-        detection_methods = { 'pattern' },
-
-        -- All the patterns used to detect root dir, when **"pattern"** is in
-        -- detection_methods
-        patterns = {'!../git', 'compile_commands.json', 'build/compile_commands.json', '^.config/nvim' },
-
-        require('telescope').load_extension 'projects',
-        vim.keymap.set('n', '<leader>ps', function()
-          require('telescope').extensions.projects.projects {}
-        end, { desc = '[P]roject [S]earch' }),
-      }
+      require('project_nvim').setup(config)
     end,
   },
 }
