6233230a546cfb468afabe8fcb859b013660a54b
Yutong Huang <yutong.huang@outlook.com>
2025-02-11 17:24:28 +0800

    better luarocks config

--- lua/common/luarocks.lua
+++ lua/common/luarocks.lua
@@ -1,5 +1,6 @@
 local sys = require 'common.system_package_manager'
 local utils = require 'common.utils'
+local is_windows = require('common.os').is_windows
 
 package.path = package.path .. ';' .. vim.fn.expand '~/.luarocks/share/lua/5.1/?/init.lua'
 package.path = package.path .. ';' .. vim.fn.expand '~/.luarocks/share/lua/5.1/?.lua'
@@ -41,8 +42,46 @@ local function install_rock(rock_name)
   utils.log_info('installed ' .. rock_name .. ' with luarocks')
 end
 
+local function get_installed_rocks()
+  local installed_rocks = {}
+  assert(luarocks_available(), 'luarocks not available')
+  -- Get a list of installed luarocks
+  local installed_output = vim.fn.system { 'luarocks', '--lua-version', '5.1', '--local', 'list', '--porcelain' }
+
+  -- Get all non-blank lines split be "\n"
+  local installed_lines = vim.tbl_filter(function(line)
+    return line ~= ''
+  end, vim.split(installed_output, '\n'))
+
+  -- Get the first element of the list
+  local installed_rock_names = vim.tbl_map(function(line)
+    return vim.split(line, '\t')[1]
+  end, installed_lines)
+
+  for _, name in ipairs(installed_rock_names) do
+    installed_rocks[name] = true
+  end
+  return installed_rocks
+end
+
+if luarocks_available() then
+  get_installed_rocks()
+end
+
+local function rock_exists(rock_name)
+  return get_installed_rocks()[rock_name]
+end
+
+local function ensure_rock_installed(rock_name)
+  if not rock_exists(rock_name) then
+    install_rock(rock_name)
+  end
+end
+
 return {
   luarocks_available = luarocks_available,
   install_luarocks = install_luarocks,
   install = install_rock,
+  exists = rock_exists,
+  ensure = ensure_rock_installed,
 }
--- lua/rocks.lua
+++ lua/rocks.lua
@@ -3,5 +3,3 @@ local rocks = require 'common.luarocks'
 if not rocks.luarocks_available() then
   rocks.install_luarocks()
 end
-
-rocks.install 'luarocks'
