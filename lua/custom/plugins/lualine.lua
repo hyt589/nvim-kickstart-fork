4313bf7d7f763df22fd5dd55ce34da5bac7b3d57
huangyutong01 <huangyutong01@baidu.com>
2025-08-11 02:46:58 +0000

    show hostname on lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -6,6 +6,22 @@ return {
     config = function()
       require('lualine').setup {
         theme = 'catppuccin',
+        sections = {
+          lualine_a = { 'hostname', 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filename' },
+          lualine_x = { 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' }
+        },
+        inactive_sections = {
+          lualine_a = { 'hostname'},
+          lualine_b = {},
+          lualine_c = { 'filename' },
+          lualine_x = { 'location' },
+          lualine_y = {},
+          lualine_z = {}
+        },
       }
     end,
   },
